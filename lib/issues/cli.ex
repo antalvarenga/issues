e6cb8fbb16fcdb79b06ba4d4060066ea80be6395
António Alvarenga <antonio.alvarenga@inferenxe.io>
2018-01-27 16:08:22 +0000

    GithubIssues module, httpoison and poison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -49,10 +49,24 @@ defmodule Issues.CLI do
     usage:  issues <user> <project> [ count | #{@default_count} ]
     """)
 
-    System.halt(0)
+    System.halt(0) #Seems to work the same with this line or without it
   end
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response
   end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({​:error​, error}) ​do​
+​ 	    {_, message} = List.keyfind(error, ​"​​message"​, 0)
+​ 	    IO.puts ​"​​Error fetching from Github: ​​#{​message​}​​"​
+​ 	    System.halt(2)
+​  ​end​
+
+
+
+
+
 end
